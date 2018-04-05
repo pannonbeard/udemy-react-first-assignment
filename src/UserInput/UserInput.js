7f8c3689e8a529522692a2154967adb41e47a31f
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-04-05 11:49:43 -0500

    added some styling

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
-const userInput = (props) =>
-  <input type='text' onChange={props.eventHandler} value={props.user}/>
+const userInput = (props) => {
+  const style = {
+    padding: '10px 15px',
+    border: '1px solid #eee',
+    font: 'inherit',
+    borderRadius: '5px',
+    marginBottom: '10px'
+  }
+
+  return(
+    <input type='text' onChange={props.eventHandler} value={props.user} style={style}/>
+  ) 
+}
 
 export default userInput
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
-const userOutput = ({user}) =>
-  <div>
-    <h2>By {user}</h2>
-    <p>blahb blha</p>
-    <p>blhab blah</p>
-  </div>
+const userOutput = ({user}) =>{
+  const style = {
+    h2: {
+      backgroundColor: 'lime',
+      margin: '0, 0, 10px, 0',
+      padding: '10px'
+    }
+  }
+
+  return(
+    <div>
+      <h2 style={style.h2}>By {user}</h2>
+      <p>blahb blha</p>
+      <p>blhab blah</p>
+    </div>
+  )
+}
+  
 
 export default userOutput
